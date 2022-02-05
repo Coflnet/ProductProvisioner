0ec3fc4113e95c95e377f955feff9d1a66976442
Äkwav <xekwavx@gmail.com>
2022-02-05 19:20:34 +0100

    reestablish compatability with the payment service

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@ using Coflnet.Payments.Client.Api;
 Console.WriteLine("Applying");
 
 var productsApi = new ProductsApi("http://" + Environment.GetEnvironmentVariable("payment_host"));
-var products = JsonSerializer.Deserialize<IEnumerable<Coflnet.Payments.Client.Model.PurchaseableProduct>>(
+var data = JsonSerializer.Deserialize<Format>(
         File.ReadAllText("products.json"),
         new JsonSerializerOptions(){PropertyNameCaseInsensitive=true});
-Console.Write(JsonSerializer.Serialize(products));
-if (products == null)
+Console.Write(JsonSerializer.Serialize(data));
+if (data == null)
     throw new Exception("products.json is invalid");
-foreach (var item in products)
+foreach (var item in data.Products)
 {
     var existing = await productsApi.ProductsPProductSlugGetAsync(item.Slug);
     if (existing == null)
@@ -27,4 +27,29 @@ foreach (var item in products)
         Console.WriteLine("Patched " + item.Slug);
     }
 }
+var existingTopups = (await productsApi.ProductsTopupGetAsync(0,10_000));
+
+foreach (var item in data.TopUps)
+{
+    var existing = existingTopups.Where(e=>e.Slug == item.Slug).FirstOrDefault();
+    if (existing == null)
+    {
+        // new 
+        await productsApi.ProductsTopupPostAsync(item);
+        Console.WriteLine("Added topup " + item.Slug);
+    }
+    else if (!item.Equals(existing))
+    {
+        // something changed, update
+        await productsApi.ProductsTopupPutAsync(item);
+        Console.WriteLine("Patched topup " + item.Slug);
+    }
+}
+
 Console.WriteLine("Applied all, have a nice day");
+
+public class Format
+{
+    public IEnumerable<Coflnet.Payments.Client.Model.PurchaseableProduct> Products {get;set;}
+    public IEnumerable<Coflnet.Payments.Client.Model.TopUpProduct> TopUps {get;set;}
+}
