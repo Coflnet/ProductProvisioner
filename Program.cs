720932bfaeaf8dc79a6bb191a6af8d93f3327863
Äkwav <xekwavx@gmail.com>
2022-04-16 15:58:52 +0200

    don't update if not certain

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text.Json;
 using Coflnet.Payments.Client.Api;
+using Coflnet.Payments.Client.Model;
 
 Console.WriteLine("Applying");
 
 var productsApi = new ProductsApi("http://" + Environment.GetEnvironmentVariable("payment_host"));
 var data = JsonSerializer.Deserialize<Format>(
         File.ReadAllText("products.json"),
-        new JsonSerializerOptions(){PropertyNameCaseInsensitive=true});
+        new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
 Console.Write(JsonSerializer.Serialize(data));
 if (data == null)
     throw new Exception("products.json is invalid");
@@ -20,25 +21,27 @@ foreach (var item in data.Products)
         await productsApi.ProductsPostAsync(item);
         Console.WriteLine("Added " + item.Slug);
     }
-    else if (!item.Equals(existing))
+    else if (!Format.AreSame(item, existing))
     {
         // something changed, update
         await productsApi.ProductsPutAsync(item);
         Console.WriteLine("Patched " + item.Slug);
+        Console.WriteLine(JsonSerializer.Serialize(item, new JsonSerializerOptions { WriteIndented = true }));
+        Console.WriteLine(JsonSerializer.Serialize(existing, new JsonSerializerOptions { WriteIndented = true }));
     }
 }
-var existingTopups = (await productsApi.ProductsTopupGetAsync(0,10_000));
+var existingTopups = (await productsApi.ProductsTopupGetAsync(0, 10_000));
 
 foreach (var item in data.TopUps)
 {
-    var existing = existingTopups.Where(e=>e.Slug == item.Slug).FirstOrDefault();
+    var existing = existingTopups?.Where(e => e.Slug == item.Slug).FirstOrDefault();
     if (existing == null)
     {
         // new 
         await productsApi.ProductsTopupPostAsync(item);
         Console.WriteLine("Added topup " + item.Slug);
     }
-    else if (!item.Equals(existing))
+    else if (!Format.AreSame(item, existing))
     {
         // something changed, update
         await productsApi.ProductsTopupPutAsync(item);
@@ -46,10 +49,29 @@ foreach (var item in data.TopUps)
     }
 }
 
-Console.WriteLine("Applied all, have a nice day");
+Console.WriteLine("\nApplied all, have a nice day");
 
 public class Format
 {
-    public IEnumerable<Coflnet.Payments.Client.Model.PurchaseableProduct> Products {get;set;}
-    public IEnumerable<Coflnet.Payments.Client.Model.TopUpProduct> TopUps {get;set;}
+    public IEnumerable<Coflnet.Payments.Client.Model.PurchaseableProduct> Products { get; set; }
+    public IEnumerable<Coflnet.Payments.Client.Model.TopUpProduct> TopUps { get; set; }
+
+    public static bool AreSame(PurchaseableProduct item, PurchaseableProduct existing)
+    {
+        return item.OwnershipSeconds == existing.OwnershipSeconds 
+                && item.Title == existing.Title 
+                && item.Cost == existing.Cost
+                && item.Type.GetValueOrDefault(0) == existing.Type.GetValueOrDefault(0)
+                && item.Description == existing.Description;
+    }
+
+    public static bool AreSame(TopUpProduct item, TopUpProduct existing)
+    {
+        return item.OwnershipSeconds == existing.OwnershipSeconds 
+                && item.Title == existing.Title 
+                && item.Cost == existing.Cost
+                && item.Type.GetValueOrDefault(0) == existing.Type.GetValueOrDefault(0)
+                && item.Description == existing.Description;
+    }
 }
+
